a8038d9f3cac9fcc2b12aa317e8d0a86cd8645f5
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-06-19 18:20:10 +0200

    form to create new product and post to db and show on homepage

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -8,4 +8,16 @@ export default async function handler(req, res) {
     const products = await Product.find();
     res.status(200).json(products);
   }
+  if(req.method === "POST"){
+    try {
+      const productData= req.body;
+      console.log("data received in server",productData);
+      const newProduct= new Product(productData);
+      await newProduct.save();
+      res.status(201).json({status: "data posted"})
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({error: error.message})
+    }
+  }
 }
---
+++ src/pages/create-new-product/index.js
@@ -0,0 +1,7 @@
+import CreateProductForm from "@/components/CreateProductForm"
+
+export default function CreateProduct() {
+  return (
+    <CreateProductForm />
+  )
+}
