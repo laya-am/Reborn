e924d1c3e4fbfb8f40a441d02caba3dcacc761d6
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-06-22 12:29:38 +0200

    add navbar to bottom of all pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import { SWRConfig } from "swr";
-
+import NavBar from '@/components/NavBar';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
@@ -8,5 +8,6 @@ export default function App({ Component, pageProps }) {
   return (
     <SWRConfig value={{ fetcher }}>
     <Component {...pageProps} />
+    <NavBar />
     </SWRConfig>
 )}
---
+++ src/pages/messages/index.js
@@ -0,0 +1,6 @@
+
+export default function messages() {
+  return (
+    <h1>your messages here</h1>
+  )
+}
