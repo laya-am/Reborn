84ec44c17ae3e585b16a33a3713455def9f2fd6c
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-07-01 14:32:21 +0200

    implement chat using Ably

--- src/pages/messages/index.js
+++ src/pages/messages/index.js
@@ -1,6 +1,12 @@
+import dynamic from 'next/dynamic'
+
+const Chat = dynamic(() => import('../../components/Chat'), { ssr: false });
 
 export default function messages() {
   return (
+    <>
     <h1>your messages here</h1>
+    <Chat />
+    </>
   )
 }
